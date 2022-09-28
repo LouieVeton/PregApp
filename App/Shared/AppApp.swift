5b9a90b035ed981e83b20d98678bc9fa9f70a580
LouieVeton <113012709+LouieVeton@users.noreply.github.com>
2022-09-28 13:39:15 +1000

    Add files via upload

---
+++ App/Shared/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  Shared
+//
+//  Created by Veton Jonuzi on 28/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
